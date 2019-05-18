3dd09633cc5a81c05b2260df9f33bff4888a322d
phantom <623601391@qq.com>
2019-05-18 23:03:07 +0800

    feat: optimze

--- src/app/delivery/delivery.component.ts
+++ src/app/delivery/delivery.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { DeliveryService } from './delivery.service';
 import { DeliveryInfo } from '../delivery-info';
+import { Toast } from 'ng-zorro-antd-mobile';
 
 @Component({
   selector: 'app-delivery',
   templateUrl: './delivery.component.html',
-  styleUrls: ['./delivery.component.less']
+  styleUrls: ['./delivery.component.less'],
+  providers: [Toast]
 })
 export class DeliveryComponent implements OnInit {
 
-  value: string = 'HC0043511';
+  value: string = '';
 
   deliveryInfo: DeliveryInfo;
 
-  constructor(private deliveryService: DeliveryService) {  }
+  constructor(private deliveryService: DeliveryService, private _toast: Toast) {  }
 
   ngOnInit() {
   }
@@ -21,6 +23,9 @@ export class DeliveryComponent implements OnInit {
   submit(value: any) {
     this.deliveryService.ping(value)
       .subscribe((info: Array<DeliveryInfo>) => {
+        if (info.length <= 0) {
+          Toast.show("未查询到结果", 1500);
+        }
         this.deliveryInfo = info.pop();
       });
   }
